69bd89ce399486cd9ebf0b668ea7625836ddd2b4
islam.bayoumy <islam.bayoumy@gmail.com>
2017-06-09 22:36:08 +0200

    created all required html files and its styles using bootstrap3

--- Chat_System/urls.py
+++ Chat_System/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from chat_app.views import AuthView 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^base/$', AuthView.test)
 ]
